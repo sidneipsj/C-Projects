6a449f76186587e8b8b81dd5507e19be6a2be19c
sidneipsj <sidneipsj@gmail.com>
2015-11-03 14:52:12 -0200

    Foi adicionado o projeto Financas

---
+++ Financas/Financas/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Financas.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Nome { get; set; }
+        [Required, EmailAddress]
+        public String Email { get; set; }
+    }
+}
