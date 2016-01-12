a6c49281d96e422d3805fb090944c9c91317488b
Sidnei Sanches <sidneipsj@gmail.com>
2016-01-12 18:05:30 -0200

    Add projeto Agenda

---
+++ BTI/BTI/Controllers/ContatoController.cs
@@ -0,0 +1,35 @@
+﻿using BTI.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTI.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: Contato
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Visualiza()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastra(CONTATO2 c)
+        {
+            ContadosDAO dao = new ContadosDAO();
+            dao.Inserir(c);
+            return View();
+        }
+
+        public ActionResult Form(CONTATO2 c)
+        {
+            return View();
+        }
+    }
+}
---
+++ BTI/BTI/Models/Setor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BTI.Models
+{
+    public class Setor
+    {
+        public int Id { get; set; }
+        public String Descricao { get; set; }
+    }
+}
